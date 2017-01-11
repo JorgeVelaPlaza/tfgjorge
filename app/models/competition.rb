7531f8ff4d6c43c31188028f2d98b9d7d875f863
Jorge Vela Plaza <velaplazajorge@gmail.com>
2017-01-11 12:57:09 +0100

    Creada la primera competicion, y la vista show para mostrarla cuando se crea

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,7 +2,7 @@ class Competition < ActiveRecord::Base
   validates :titulo, presence: true, length: { minimum: 3, maximum: 100}
   validates :descripcion, presence: true
   validates :premio, presence: true
-  validates :dificultad, inclusion: { in: ["facil", "medio", "experto"] }
+  validates :dificultad, inclusion: { in: ["Fácil", "Medio", "Experto"] }
   validates :dificultad, presence: true
   validates :deadline, presence: true
 end
