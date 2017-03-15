176c08a58a9b7d52fa896983eb023fe90d9ba71c
Jorge Vela Plaza <velaplazajorge@gmail.com>
2017-03-15 19:54:46 +0100

    Test Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :rvm_ruby_version, '1.28.0'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
